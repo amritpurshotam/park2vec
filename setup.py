717afd39f417b07bea45b998dd6d9d02c01058c0
Amrit Purshotam <amrit.purshotam@gmail.com>
2021-06-12 14:45:09 +0200

    Add dev setup

--- setup.py
+++ setup.py
@@ -7,18 +7,19 @@ setup(
     name="park2vec",
     version="0.1.0",
     packages=find_packages(include=["src"]),
+    python_requires=">=3.8",
     install_requires=install_requires,
     extras_require={
         "dev": [
             "bandit==1.7.0",
             "black==20.8b1",
-            "flake8==3.9.1",
+            "flake8==3.9.2",
             "flake8-bandit==2.1.2",
             "flake8-bugbear==21.4.3",
             "flake8-builtins==1.5.3",
-            "flake8-comprehensions==3.4.0",
+            "flake8-comprehensions==3.5.0",
             "isort==5.8.0",
         ],
-        "test": ["pytest==6.2.3"],
+        "test": ["pytest==6.2.4"],
     },
 )
