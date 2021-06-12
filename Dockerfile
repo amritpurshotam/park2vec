bfecc3a6532ec3798a25e9d6d8e63e5693595217
Amrit Purshotam <amrit.purshotam@gmail.com>
2021-06-12 14:40:08 +0200

    Switch to openstreetmap tile server

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM postgis/postgis:13-3.1
-
-COPY load-extensions.sh /docker-entrypoint-initdb.d/
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="park2vec",
+    version="0.1.0",
+    packages=find_packages(include=["src"]),
+    install_requires=install_requires,
+    extras_require={
+        "dev": [
+            "bandit==1.7.0",
+            "black==20.8b1",
+            "flake8==3.9.1",
+            "flake8-bandit==2.1.2",
+            "flake8-bugbear==21.4.3",
+            "flake8-builtins==1.5.3",
+            "flake8-comprehensions==3.4.0",
+            "isort==5.8.0",
+        ],
+        "test": ["pytest==6.2.3"],
+    },
+)
